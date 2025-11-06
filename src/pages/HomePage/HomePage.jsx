2c09d2a8c9c4325fbce8bf32a59e684cbd9145bf
Milad Afzali <miladsteam455@gmail.com>
2025-11-07 01:42:24 +0330

    add DealsSection to HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
-import HeroBanner from "../../components/HeroBanner/HeroBanner";
 import styles from "./HomePage.module.css";
+import HeroBanner from "../../components/HeroBanner/HeroBanner";
+import DealsSection from "../../components/DealsSection/DealsSection";
 
 export default function HomePage() {
   return (
     <div className={styles.home}>
       <HeroBanner />
+      <DealsSection />
     </div>
   );
 }
