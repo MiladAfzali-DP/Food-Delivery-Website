f09609e790a1873ebf398532f626db0c537be20b
Milad Afzali <miladsteam455@gmail.com>
2025-11-10 21:23:41 +0330

    change dealCard to Reusable Component

--- src/components/DealCard/DealCard.jsx
+++ src/components/DealCard/DealCard.jsx
@@ -1,16 +1,38 @@
 import styles from "./DealCard.module.css";
+import Title from "../ui/Title/Title";
+const badgeStyle = {
+  backgroundColor: "#03081F",
+  color: "white",
+  top: "0",
+};
 export default function DealCard({
-  percentOffer = "-",
-  bgImage = "",
-  children,
+  className = "",
+  badgeCustomStyle = { bottom: "none", right: "1rem", left: "none" },
+  badgeText = "-",
+  bgImage = null,
+  title = "",
+  subTitle = "",
+  children = "",
 }) {
   return (
-    <div className={styles.dealCard}>
-      <span className={styles.percentOffer}>{percentOffer}%</span>
-      <img src={bgImage} alt="" />
+    <div className={`${styles.dealCard} ${className}`}>
+      <span
+        className={styles.badge}
+        style={{ ...badgeStyle, ...badgeCustomStyle }}
+      >
+        {badgeText}%
+      </span>
+      {bgImage ? (
+        <img src={bgImage} alt="" />
+      ) : (
+        <Title customClass={styles.notFoundImage}>
+          Please Add your Image Src
+        </Title>
+      )}
       <div className={styles.content}>
-        <p>Restaurant</p>
-        <h4>{children}</h4>
+        <p>{subTitle}</p>
+        <h4>{title}</h4>
+        {children}
       </div>
     </div>
   );
